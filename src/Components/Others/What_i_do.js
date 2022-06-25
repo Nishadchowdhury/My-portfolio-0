92fd35aace371a4e7533e14f1d0e3642f4a94230
Nishadchowdhury <Nishadhj111@gmail.com>
2022-06-26 00:20:40 +0600

    add what i do icon of web

--- src/Components/Others/What_i_do.js
+++ src/Components/Others/What_i_do.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { } from '@fortawesome/free-solid-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 
 const What_i_do = () => {
     return (
-        <div className='px-16' >
-            <h1 className='border-b-[3px] border-blue-400 inline-block ' >What i do</h1>
+        <div className='px-16 text-white' >
+            <h1 className='border-b-[3px] border-blue-400 inline-block  ' >What i do</h1>
 
 
             <div className='grid grid-cols-2 gap-4'>
 
-                <div className='' >
-                    <span className='inline-block' >
-                        <i>  </i>
+                <div className='flex ' >
+                    <span className='inline-block text-blue-500 text-3xl m-4  ' >
+                        <FontAwesomeIcon icon={faGlobe} />
                     </span>
 
-                    <p>
+                    <p className='pt-4'>
+                        <span className='block' >Web Development</span>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, perspiciatis doloribus corporis repudiandae soluta!
                     </p>
                 </div>
-
 
             </div>
 
