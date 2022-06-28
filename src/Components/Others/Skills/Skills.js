84db8241143b056aba1e3706be4bc92bc0ccb095
Nishadchowdhury <Nishadhj111@gmail.com>
2022-06-28 12:37:05 +0600

    more add of animation and my scill section

--- src/Components/Others/Skills/Skills.js
+++ src/Components/Others/Skills/Skills.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Skills.scss'
-
 const Skills = () => {
 
 
 
+    const options = {
+        radius: 230,
+        maxSpeed: 'fast',
+        initSpeed: 'slow',
+        direction: 500,
+        keep: true,
+    };
+
+    const texts = [
+        'HTML', 'CSS', 'JAVASCRIPT',
+        'MONGOOSE', 'REACT', 'SASS',
+        'NODEJS', 'MONGODB',
+        'EXPRESS', 'NoSQL', 'JQUERY'
+    ];
+
+
+    useEffect(() => {
+        const TagCloud = require('TagCloud');
+
+        const container = '.tagcloud';
+
+        TagCloud(container, texts, options);
+
+    }, [])
+
+
+
+
     return (
-        <div>
+        <>
+            <div className=' flex justify-between '>
 
-            my Skills is here
+                <div className='w-1/2 bg-red-400 ' >
+                    <span className='text-white text-5xl font-extrabold' >  Me &#38; My self </span>
+                </div>
 
-        </div>
-    );
+
+                <div className='w-1/2 flex justify-center items-center '  >
+                    <div className="tagcloud"></div>
+                </div>
+
+            </div>
+        </>
+    )
+
 };
 
 export default Skills;
